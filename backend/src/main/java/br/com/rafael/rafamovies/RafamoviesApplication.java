4a3d7d2747050530a6cb577dc0b0ac729e78c13b
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-01-13 23:15:12 -0300

    Project Created

---
+++ backend/src/main/java/br/com/rafael/rafamovies/RafamoviesApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.rafamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafamoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafamoviesApplication.class, args);
+	}
+
+}
